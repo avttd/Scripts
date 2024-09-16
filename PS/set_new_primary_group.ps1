4e4f40037a12739b6e35544133e4e1f10bf0772b
Aleksej Vedernikov <aovedernikov@gmail.com>
2024-09-16 14:23:44 +0200

    Скрипт меняющий основную группу у пользователя в AD и удаляющий все остальные

---
+++ PS/set_new_primary_group.ps1
@@ -0,0 +1,26 @@
+﻿#путь до OU с уволенными сотрудниками
+$ouDN = "OU=USERS FIRED,DC=COMPANY,DC=local"
+
+#путь до группы с уволенными сотрудниками
+$firedCN = "CN=Fired_users,DC=COMPANY,DC=local"
+
+# Получаем RID новой основной группы FIRED_USERS
+$groupRID = Get-ADGroup -Identity FIRED_USERS -Property primaryGroupToken | Select-Object -ExpandProperty primaryGroupToken
+
+# Получаем всех пользователей
+$users = Get-ADUser -Filter * -SearchBase $ouDN -Properties MemberOf
+
+foreach ($user in $users) {
+
+#Добавляем пользователя в группу FIRED_USERS
+    Add-ADGroupMember -Identity "FIRED_USERS" -Members $user.SamAccountName
+
+#укажем группу в качестве основной для пользователя:
+   Set-ADUser -Identity $user.SamAccountName -Replace @{primaryGroupID = $groupRID}
+
+# Удаляем пользователя из всех групп, кроме FIRED_USERS
+$groups = $user.MemberOf | Where-Object { $_ -ne $firedCN }
+    foreach ($group in $groups) {
+        Remove-ADGroupMember -Identity $group -Members $user.SamAccountName -Confirm:$false
+    }
+}
