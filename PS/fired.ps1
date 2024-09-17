9b448386f2654c1933fc713d20be95d3553b06f2
Aleksej Vedernikov <aovedernikov@gmail.com>
2024-09-17 15:13:34 +0200

    Скрипт позволяющий в связке сделать exchange почту увольняемого сотрудника общим ящиком, а саму учетку перенести в группу и OU уволенных + у пользователя из групп остается только группа с уволенными и она является главной

---
+++ PS/fired.ps1
@@ -0,0 +1,38 @@
+﻿[CmdletBinding()]
+Param (
+[Parameter (Mandatory=$true, Position=1)]
+[string] $userLogin
+)
+
+$ouDN = "OU=USERS FIRED,DC=COMPANY,DC=local"
+$firedCN = "CN=Fired_users,OU=USERS FIRED,DC=COMPANY,DC=local"
+
+# Получаем RID для группы Fired_users
+$groupRID = Get-ADGroup -Identity Fired_users -Property primaryGroupToken | Select-Object -ExpandProperty primaryGroupToken
+
+# 1 Делаем учетку общим ящиком
+Set-Mailbox -Identity $userLogin -type shared
+
+# 2 Переименовываем учетку
+#$user = (Get-ADUser -Identity $userLogin -Properties DisplayName).DisplayName
+$user = (Get-ADUser -Identity $userLogin -Properties DisplayName).Name
+$newDisplayName = "Archive-" + $user
+Set-ADUser -Identity $userLogin -DisplayName $newDisplayName
+
+# 3 Переносим учетку в OU USERS FIRED
+$userDN = (Get-ADUser -Identity $userLogin -Properties DisplayName).DistinguishedName
+Move-ADObject -Identity $userDN -TargetPath "OU=USERS FIRED,DC=COMPANY,DC=local"
+
+# 4 Добавляем пользователя в группу Fired_users
+Add-ADGroupMember -Identity "Fired_users" -Members $userLogin
+
+# Указываем группу в качестве основной для пользователя
+Set-ADUser -Identity $userLogin -Replace @{primaryGroupID = $groupRID}
+
+# Удаляем пользователя из всех групп, кроме Fired_users
+$user = Get-ADUser -Identity $userLogin -Properties MemberOf
+$groups = $user.MemberOf | Where-Object { $_ -ne $firedCN }
+
+foreach ($group in $groups) {
+        Remove-ADGroupMember -Identity $group -Members $userLogin -Confirm:$false
+}
